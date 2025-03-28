2878c9640cb6f31e180525c6a275d5d320f6aa37
Uriel Engelberg & Lucas Tjor <uelt@localhost>
2025-03-28 10:40:37 -0300

    Eliminar test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-function armarRequestString() {
-    const requestUrl = new URL("https://www.omdbapi.com/");
-    requestUrl.searchParams.append("apikey", "aaahhh");
-    console.log(requestUrl.href);
-}
-armarRequestString();
