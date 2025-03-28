a5b0a5e7abec5cc37bd82d091b2c6b6561881b62
Uriel Engelberg & Lucas Tjor <uelt@localhost>
2025-03-28 08:16:50 -0300

    Ej01 terminado

---
+++ src/ej01.js
@@ -0,0 +1,8 @@
+let textoEntrada01 = "Escuela", textoEntrada02 = "ORT"; 
+let textoSalida = ""; 
+textoSalida = concatInvert(textoEntrada01, textoEntrada02); 
+console.clear(); // Borra la pantalla en la consola. 
+console.log(`Textos de Entrada: "${textoEntrada01}" y "${textoEntrada02}"`); console.log(`Texto de Salida: "${textoSalida}"`); 
+function concatInvert (texto1, texto2) {
+    return texto1.concat("", texto2).split("").reverse().join("");
+}
