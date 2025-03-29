3e7406147ccb9c36e295e192013a446501e358b3
Uriel Engelberg & Lucas Tjor <uelt@localhost>
2025-03-29 16:47:55 -0300

    Terminar ej08

--- src/ej08.js
+++ src/ej08.js
@@ -1,5 +1,11 @@
-import {OMDBSearchByPage} from './modules/omdb-wrapper.js'
+import {OMDBSearchByPage, OMDBSearchComplete, OMDBGetByImdbID} from './modules/omdb-wrapper.js'
 let resultado = null;
 // Test de la función OMDBSearchByPage..
 resultado = await OMDBSearchByPage("cars", 1);
 console.log("OMDBSearchByPage", resultado);
+// Test de la función OMDBSearchComplete..
+resultado = await OMDBSearchComplete("cars");
+console.log("OMDBSearchComplete", resultado);
+// Test de la función OMDBGetByImdbID..
+resultado = await OMDBGetByImdbID("tt0317219");
+console.log("OMDBGetByImdbID", resultado);
--- src/modules/omdb-wrapper.js
+++ src/modules/omdb-wrapper.js
@@ -1,27 +1,39 @@
-/* Módulo OMDBWrapper*/
+/*
+* Módulo OMDBWrapper
+* Se asume que si la web cae, se devuelve returnObject original
+*/
 import 'dotenv/config';
 import axios from "axios";
 
 const APIKEY = process.env.APIKEY; // Poné tu APIKEY, esta no funciona.
-const ENDPOINT = "https://www.omdbapi.com/";
 
-function armarRequestUrl() {
+function armarRequestUrl(queryParams) {
     const requestUrl = new URL("https://www.omdbapi.com/");
     requestUrl.searchParams.append("apikey", APIKEY);
-    return requestUrl;
+    for (const key in queryParams)
+        requestUrl.searchParams.append(key, queryParams[key]);
+    return requestUrl.href;
 }
 
-
 const OMDBSearchByPage = async (searchText, page = 1) => {
-    const requestString = armarRequestUrl();
-    armarRequestUrl.append("s", searchText);
-    armarRequestUrl.append("page", page);
     let returnObject = {
         respuesta : false,
         cantidadTotal : 0,
         datos : []
     };
-    // No seas vago, acá hay que hacer el cuerpo de la función!!!
+    const requestUrl = armarRequestUrl({ s: searchText, page: page });
+
+    try {
+        const response = await axios.get(requestUrl);
+        returnObject.respuesta = response.data.Response === "True";
+        if (returnObject.respuesta) {
+            returnObject.cantidadTotal = parseInt(response.data.totalResults);
+            returnObject.datos = response.data.Search;
+        }
+    } catch (error) {
+        console.error("No se pudo conectar a la API:", error);
+    }
+
     return returnObject;
 };
 const OMDBSearchComplete = async (searchText) => {
@@ -30,7 +42,19 @@ const OMDBSearchComplete = async (searchText) => {
         cantidadTotal : 0,
         datos : []
     };
-    // No seas vago, acá hay que hacer el cuerpo de la función!!!
+    const requestUrl = armarRequestUrl({ s: searchText });
+
+    try {
+        const response = await axios.get(requestUrl);
+        returnObject.respuesta = response.data.Response === "True";
+        if (returnObject.respuesta) {
+            returnObject.cantidadTotal = parseInt(response.data.totalResults);
+            returnObject.datos = response.data.Search;
+        }
+    } catch (error) {
+        console.error("No se pudo conectar a la API:", error);
+    }
+
     return returnObject;
 };
 const OMDBGetByImdbID = async (imdbID) => {
@@ -39,7 +63,20 @@ const OMDBGetByImdbID = async (imdbID) => {
         cantidadTotal : 0,
         datos : {}
     };
-    // No seas vago, acá hay que hacer el cuerpo de la función!!!
+    const requestUrl = armarRequestUrl({ i: imdbID });
+
+    try {
+        const response = await axios.get(requestUrl);
+        returnObject.respuesta = response.data.Response === "True";
+        if (returnObject.respuesta) {
+            delete response.data.Response
+            returnObject.datos = response.data;
+            returnObject.cantidadTotal = 1;
+        }
+    } catch (error) {
+        console.error("No se pudo conectar a la API:", error);
+    }
+
     return returnObject;
 };
 // Exporto todo lo que yo quiero exponer del módulo:
